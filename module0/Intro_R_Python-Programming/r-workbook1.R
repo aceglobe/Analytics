1eca050fbb6d74612742b3a5ada60bc9263d0fb3
Helena Adu Asare <maccarthy.stclair@gmail.com>
2026-01-31 19:48:30 -0500

    Adding Programming in R & Python

---
+++ module0/Intro_R_Python-Programming/r-workbook1.R
@@ -0,0 +1,39 @@
+#Our First R Function
+#Written Dr. Opoku
+# This calculator computes the square of
+# a vector
+#It takes the vector as an input 
+# from the user
+
+koo_fun <- function (x){
+#x = x
+t= x^2
+}
+(koo_fun(4))
+
+my_gid <- function ()
+           {
+             u <- c(1,4,6)
+             v <- u^2
+           }
+(my_gid())
+
+#R internal functions
+(round (2.6567))
+mean(10:100)
+
+#using concatenate for vectors in R
+x = c(2,1,4)
+
+#Using R packages
+install.packages("ggplot2")
+install.packages("tidyr")
+library("tidyr")
+library("ggplot2")
+
+(x <- c(-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1))
+(y <- x^2)
+
+#plotting data points with qplots
+qplot(x,y)
+
